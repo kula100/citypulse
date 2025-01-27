b3ef98a1641c0751d3b41b0dc59bb73f190805f1
dharamdudi <dudidharam12i@gmail.com>
2025-01-27 11:57:39 +0530

    updated the ingestion workflow

--- dags/ingestion/yelp_data_ingestion.py
+++ dags/ingestion/yelp_data_ingestion.py
@@ -1,56 +1,89 @@
 import os
+import glob
 import snowflake.connector
+from utils import logger
 
-# Retrieve environment variables
-user = os.getenv("SNOWFLAKE_USER")
-password = os.getenv("SNOWFLAKE_PASSWORD")
-account = os.getenv("SNOWFLAKE_ACCOUNT")
-warehouse = os.getenv("SNOWFLAKE_WAREHOUSE")
-database = os.getenv("SNOWFLAKE_DATABASE")
-schema = os.getenv("SNOWFLAKE_SCHEMA")
-stage = os.getenv("SNOWFLAKE_STAGE")
-target_dir = os.getenv("TARGET_DIR")
+logger = logger()
 
-# Connect to Snowflake
-conn = snowflake.connector.connect(
-    user=user,
-    password=password,
-    account=account,
-    warehouse=warehouse,
-    database=database,
-    schema=schema
-)
+# Snowflake connection details
+SNOWFLAKE_ACCOUNT = os.getenv("SNOWFLAKE_ACCOUNT")
+SNOWFLAKE_USER = os.getenv("SNOWFLAKE_USER")
+SNOWFLAKE_PASSWORD = os.getenv("SNOWFLAKE_PASSWORD")
+SNOWFLAKE_WAREHOUSE = os.getenv("SNOWFLAKE_WAREHOUSE")
+SNOWFLAKE_ROLE = "prod_role"
+SNOWFLAKE_DATABASE = "raw"
+SNOWFLAKE_SCHEMA = "yelp"
+SNOWFLAKE_STAGE = os.getenv("SNOWFLAKE_STAGE")
+TARGET_DIR = os.getenv("TARGET_DIR")
 
-cursor = conn.cursor()
+# tables = {"tip", "business", "checkin", "user", "review"}
+tables = {"tip", "business", "checkin"}
 
-# Function to upload files to Snowflake stage
-def upload_file_to_stage(file_path, stage):
+def upload_files_to_stage(cursor, table_name, files):
+    """
+        Uploads files to the Snowflake stage
+        Args:
+            cursor: Snowflake connection cursor
+            table_name: Name of the table
+            files: List of files
+    """
+    for file in files:
+        logger.info("Uploading %s to %s stage...", file, SNOWFLAKE_STAGE)
+        put_query = f"PUT file://{file} @{SNOWFLAKE_STAGE}/{table_name}/"
+        cursor.execute(put_query)
+        logger.info("File %s uploaded to %s stage", file, SNOWFLAKE_STAGE)
+
+def copy_files_to_table(cursor, table_name):
+    """
+        Executes COPY INTO command to load data into the table.
+        Args:
+            cursor: Snowflake connection cursor
+            table_name: Name of the table
+    """
+    logger.info("Copying files into %s ...", table_name)
+    copy_query = f"""
+        COPY INTO {table_name}
+        FROM (SELECT PARSE_JSON($1), CURRENT_TIMESTAMP
+            FROM @{SNOWFLAKE_STAGE}/{table_name})
+        FILE_FORMAT = (TYPE = PARQUET COMPRESSION = GZIP)
+    """
+    cursor.execute(copy_query)
+    logger.info("Files copied into the table %s", table_name)
+
+def main():
+    # Connect to Snowflake
+    conn = snowflake.connector.connect(
+        user=SNOWFLAKE_USER,
+        password=SNOWFLAKE_PASSWORD,
+        account=SNOWFLAKE_ACCOUNT,
+        warehouse=SNOWFLAKE_WAREHOUSE,
+        database=SNOWFLAKE_DATABASE,
+        schema=SNOWFLAKE_SCHEMA,
+    )
+
+    cursor = conn.cursor()
+
     try:
-        print(f"Uploading {file_path} to Snowflake stage {stage}...")
-        cursor.execute(f"PUT file://{file_path} @{stage}")
-        print(f"Uploaded {file_path} successfully.")
-    except Exception as e:
-        print(f"Error uploading {file_path}: {str(e)}")
+        for table in tables:
+            table_name = f"raw_{table}"
+            create_table_query = f"""
+                CREATE TABLE IF NOT EXISTS {table_name} (
+                    raw_data VARIANT,
+                    ingestion_datetime TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP
+                )
+            """
+            logger.info("Creating %s table: %s", table_name, create_table_query)
+            cursor.execute(create_table_query)
+            logger.info("%s table Created", table_name)
+            files = glob.glob(os.path.join(TARGET_DIR, f"{table}_*.parquet"))
+            if files:
+                upload_files_to_stage(cursor, table_name, files)
+                copy_files_to_table(cursor, table)
+            else:
+                logger.info("No files found for %s", table)
+    finally:
+        cursor.close()
+        conn.close()
 
-# Function to perform the COPY INTO operation
-def copy_into_table(stage, table):
-    try:
-        print(f"Copying data from stage {stage} into table {table}...")
-        cursor.execute(f"COPY INTO {table} FROM @{stage} FILE_FORMAT = (TYPE = 'CSV')")
-        print("Data copied successfully.")
-    except Exception as e:
-        print(f"Error during COPY INTO: {str(e)}")
-
-# Upload dataset files to Snowflake stage
-dataset_dir = target_dir
-for file_name in os.listdir(dataset_dir):
-    file_path = os.path.join(dataset_dir, file_name)
-    if file_name.endswith('.csv'):  # Assuming dataset files are in CSV format
-        upload_file_to_stage(file_path, stage)
-
-# Perform COPY INTO operation (adjust table name accordingly)
-copy_into_table(stage, "your_table_name")
-
-# Close connection
-cursor.close()
-conn.close()
+if __name__ == "__main__":
+    main()
