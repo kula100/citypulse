085eec1a8dec9ba653e22ff8b059419ed9a59468
ddudi07 <ddudi@earnest.com>
2025-04-06 23:01:49 +0530

    fixing download

--- dags/dbt_artifacts/download_artifacts.py
+++ dags/dbt_artifacts/download_artifacts.py
@@ -11,9 +11,12 @@ project = "citypulse"
 version = "latest"
 
 download_folder = os.environ.get("DBT_ARTIFACT_DIR", "target-deferred")
+target_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "dbt", download_folder))
+
 
 for artifact in artifacts:
     storage_path = f"{project}/{version}/{artifact}"
     res = supabase.storage.from_(bucket).download(storage_path)
-    with open(f"../../dbt/{download_folder}/{artifact}", "wb") as f:
+    path = os.path.join(target_dir, artifact)
+    with open(path, "wb") as f:
         f.write(res)
